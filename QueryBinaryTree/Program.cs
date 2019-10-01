e825c7f27024a3ce74754040cd6cfe4d55439cb9
Joaquim Barreto <joaquim@barreto.co>
2019-10-01 10:14:13 +0200

    Done

--- QueryBinaryTree/Program.cs
+++ QueryBinaryTree/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BinaryTree;
 
 namespace QueryBinaryTree
 {
@@ -10,7 +11,68 @@ namespace QueryBinaryTree
     {
         static void doWork()
         {
-            // TODO:
+            Tree<Employee> empTree = new Tree<Employee>(new Employee { Id = 1, FirstName = "Kim", LastName = "Abercrombie", Department = "IT" });
+            empTree.Insert(new Employee { Id = 2, FirstName = "Jeff", LastName = "Hay", Department = "Marketing" });
+            empTree.Insert(new Employee { Id = 3, FirstName = "Charlie", LastName = "Herb", Department = "IT" });
+            empTree.Insert(new Employee { Id = 4, FirstName = "Chris", LastName = "Preston", Department = "Sales" });
+            empTree.Insert(new Employee { Id = 5, FirstName = "Dave", LastName = "Barnett", Department = "Sales" });
+            empTree.Insert(new Employee { Id = 6, FirstName = "Tim", LastName = "Litton", Department = "Marketing" });
+
+            Console.WriteLine("All Employees");
+            var allEmployees = from e in empTree.ToList<Employee>() select e;
+            foreach (var emp in allEmployees)
+            {
+                Console.WriteLine(emp);
+            }
+
+            empTree.Insert(new Employee
+            {
+                Id = 7,
+                FirstName = "David",
+                LastName = "Simpson",
+                Department = "IT"
+            });
+
+            Console.WriteLine();
+            Console.WriteLine("Employee added.");
+            Console.WriteLine("All Employees");
+            foreach (var emp in allEmployees)
+            {
+                Console.WriteLine(emp);
+            }
+
+
+            //Console.WriteLine("List of departments:");
+            //// var depts = empTree.Select(data => data.Department).Distinct();
+            //var depts = (from d in empTree select d.Department).Distinct();
+            //foreach (var dept in depts)
+            //{
+            //    Console.WriteLine($"Department: {dept}");
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("Employees in the IT Department:");
+            //// var ITEmployees = empTree.Where(emp => String.Equals(emp.Department, "IT")).Select(e => e);
+            //var ITEmployees = from e in empTree where String.Equals(e.Department, "IT") select e;
+            //foreach (var e in ITEmployees)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            //Console.WriteLine("");
+            //Console.WriteLine("All employees grouped by departments:");
+            //// var employeesByDept = empTree.GroupBy(emp => emp.Department);
+            //var employeesByDept = from e in empTree group e by e.Department;
+            //foreach (var dept in employeesByDept)
+            //{
+            //    Console.WriteLine($"Department: {dept.Key}");
+            //    foreach (var e in dept)
+            //    {
+            //        Console.WriteLine($"\t{e.FirstName} {e.LastName}");
+            //    }
+            //}
+
+
         }
 
         static void Main()
